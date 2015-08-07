eb71d84bba693b5af840c40f47d7a4f584576876
Kenta Ishiguro <guro.12307910@gmail.com>
2015-08-07 16:46:09 +0900

    機能を実装

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 #Twitter for SubilimeText
+It uses tweepy(https://github.com/tweepy/tweepy)
+
+#Usage
+default_setting.json
+データを正しく入力しておく.
+site\_packages\_pathでtweepyのパスを通すので,tweepyのある場所を指定.
+Selected string is command.
+- commands
+	- "tl:" 自身のタイムラインを取得
+	- 何も指定しない場合: そのままツイートする
+	- "rp:_tweet id_,_Value_" リプライを送る
+	- "rt:_tweet id_," リツイートする
+	- "fv:_tweet id_" お気に入り登録する
+	- "dl:_tweet id_" ツイートを削除する
+	- "ll:" リストの一覧を取得
+	- "lt:_screen name_,_list name_,"リストのタイムラインを取得
--- SublimeTweet.py
+++ SublimeTweet.py
@@ -36,31 +36,129 @@ class TweetCommand(sublime_plugin.TextCommand):
 
 		self.api = tweepy.API(auth)
 
-		if len(text) > 1:
-			HEAD = text[0]
-			OPR = text[1]
-			if HEAD == 't' and OPR == ':':
-				timeline = self.get_time_line()
+		if len(text) > 2:
+			HEAD = text[:2]
+			OPR = text[2]
+			op = len(HEAD) + len(OPR)
+
+			if HEAD == 'tl' and OPR == ':':
+				timeline = self.get_my()
 				for tweet in timeline:
 					self.view.insert(edit, 0, tweet)
 				self.view.insert(edit, 0, "\n")
-			elif HEAD == 'r' and OPR == ':':
-				endpoint = text.find(',')
-				if endpoint != -1:
-					in_reply_to_status_id = text[2:endpoint]
-					self.view.insert(edit, 0, self.tweet(text[endpoint+1:], in_reply_to_status_id))
-			else:
-				self.view.insert(edit, 0, self.tweet(text))
 
+			elif HEAD == 'rp' and OPR == ':':
+				firstarg = text.find(',')
+				if firstarg != -1:
+					in_reply_to_status_id = text[op:firstarg]
+					self.view.insert(edit, 0, self.tweet(text[firstarg+1:],
+					 in_reply_to_status_id))
+
+			elif HEAD == 'll' and OPR == ':':
+				lists = self.get_list()
+				for val in lists:
+					self.view.insert(edit, 0, val)
+				self.view.insert(edit, 0, "\n")
+
+			elif HEAD == 'lt' and OPR == ':':
+				timeline = []
+				firstarg = text.find(',')
+				if firstarg != -1 and len(text) > firstarg + 1:
+					screen_name = text[op:firstarg]
+					text = text[firstarg+1:]
+					secondarg = text.find(',')
+					if secondarg != -1:
+						listid = text[:secondarg]
+						timeline = self.get_list_timeline(screen_name, listid)
+						for tweet in timeline:
+							self.view.insert(edit, 0, tweet)
+				self.view.insert(edit, 0, "\n")
+
+			elif HEAD == 'rt' and OPR ==':':
+				firstarg = text.find(',')
+				if firstarg != -1:
+					tweet_id = text[op:firstarg]
+					self.view.insert(edit, 0, self.retweet(tweet_id))
+
+			elif HEAD == 'fv' and OPR ==':':
+				firstarg = text.find(',')
+				if firstarg != -1:
+					tweet_id = text[op:firstarg]
+					self.view.insert(edit, 0, self.favorite(tweet_id))
+
+			elif HEAD == 'dl' and OPR == ':':
+				firstarg = text.find(',')
+				if firstarg != -1:
+					tweet_id = text[op:firstarg]
+					self.view.insert(edit, 0, self.destroy_tweet(tweet_id))
+
+			else :
+				self.view.insert(edit, 0, self.tweet(text))
+		else :
+			self.view.insert(edit, 0, self.tweet(text))
+
+	def tweet_detail(self, tweet_id):
+		return self.api.get_status(tweet_id)
 
 	def tweet(self, text, reply_id = None):
 		self.api.update_status(status = text, in_reply_to_status_id = reply_id)
-		return "finish"
+		return "finish\n"
 
-	def get_time_line(self):
-		public_tweets = self.api.home_timeline()
+	def destroy_tweet(self, tweet_id):
+		try:
+			self.api.destroy_status(tweet_id)
+			return "finish\n"
+		except:
+			return "error\n"
+
+	def retweet(self, tweet_id):
+		try:
+			self.api.retweet(tweet_id)
+			return "finish\n"
+		except:
+			return "error\n"
+
+	def favorite(self, tweet_id):
+		favorited = self.tweet_detail(tweet_id).favorited
+		if not favorited:
+			return self.create_fav(tweet_id)
+		else:
+			return self.destroy_fav(tweet_id)			
+
+	def create_fav(self, tweet_id):
+		try:
+			self.api.create_favorite(tweet_id)
+			return "finish\n"
+		except:
+			return "error\n"
+
+	def destroy_fav(self, tweet_id):
+		try:
+			self.api.destroy_favorite(tweet_id)
+			return "finish\n"
+		except:
+			return "error\n"
+
+	def get_timeline(self, public_tweets):
 		timeline = []
 		for tweet in public_tweets:
-			timeline.append(str(tweet.id) + ": @" + tweet.user.screen_name + " " + tweet.text + "\n")
+			timeline.append(str(tweet.id) + ": @" + tweet.user.screen_name + 
+				" " + tweet.text + "\n")
 		timeline.reverse()
 		return timeline
+
+	def get_list(self):
+		Lists = self.api.lists_all()
+		listids = []
+		for val in Lists:
+			listids.append(val.full_name + ": " + val.name + "\n")
+		return listids
+
+	def get_my(self):
+		public_tweets = self.api.home_timeline()
+		return self.get_timeline(public_tweets)
+
+	def get_list_timeline(self, screen_name, listid):
+		public_tweets = self.api.list_timeline(owner_screen_name=screen_name, 
+			slug=listid)
+		return self.get_timeline(public_tweets)
